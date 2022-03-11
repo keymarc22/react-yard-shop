0973572bf9914630e03accaf0d2bdef79c0d5629
keymarc22 <cloraltkeymar22@gmail.com>
2022-03-10 22:23:13 -0400

    using formdata

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Logo } from '@components/Logo';
 import '@styles/Login.scss';
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data)
+  }
+
   return (
     <div className="login">
       <div className="form-container login-page">
         <Logo />
 
-        <form action="/" className="form">
-          <label for="email" className="label">Email address</label>
-          <input type="email" placeholder="cloraltkeymar22@gmail.com" id="email" className="input input-email" />
-          <label for="password" className="label">Password</label>
-          <input type="password" placeholder="******" id="password" className="input input-password" />
-          <input type="submit" value="Log in" className="primary-button login-button" />
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">Email address</label>
+          <input type="email" placeholder="cloraltkeymar22@gmail.com" name="email" className="input input-email" />
+          <label htmlFor="password" className="label">Password</label>
+          <input type="password" placeholder="******" name="password" className="input input-password" />
+          <button
+            className="primary-button login-button"
+            onClick={handleSubmit}
+          >
+            Log in
+          </button>
           <a className="forgot-password" href="/">Forgot my password</a>
         </form>
 
-        <button className="secondary-button signup-button">Sign up</button>
+        <button
+          className="secondary-button signup-button"
+          type='button'
+        >
+          Sign up
+        </button>
       </div>
     </div>
   );
